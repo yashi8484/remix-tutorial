644c508be556bb48a114dd0a62b7f2b879340a6d
yashi8484 <kobayashi@giftee.co>
2023-12-28 12:36:54 +0900

    Cancel Button

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,7 +1,7 @@
 import { ActionFunctionArgs, LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getContact, updateContact } from "../data";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
 
 export const loader = async ({
   params,
@@ -27,6 +27,7 @@ export const action = async ({
 
 export default function EditContact() {
   const { contact } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
   return (
     <Form id="contact-form" method="post">
@@ -76,7 +77,7 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button onClick={() => navigate(-1)} type="button">Cancel</button>
       </p>
     </Form>
   );
