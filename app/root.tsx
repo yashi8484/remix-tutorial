be6afc13cdca3dd882106980f43cd393fca5b8db
yashi8484 <kobayashi@giftee.co>
2023-12-28 12:41:43 +0900

    URLSearchParams and GET Submissions

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import {
   Form,
@@ -20,8 +20,12 @@ export const links: LinksFunction = () => [
   {rel: "stylesheet", href: appStyleHref}
 ]
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 }
 
