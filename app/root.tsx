6887526650b16a0cfd9cc173c9f415fb741dcb19
yashi8484 <kobayashi@giftee.co>
2023-12-28 12:15:08 +0900

    Active Link Styling

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -61,20 +62,29 @@ export default function App() {
             {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
-              <li key={contact.id}>
-                <Link to={`contacts/${contact.id}`}>
-                  {contact.first || contact.last ? (
-                    <>
-                      {contact.first} {contact.last}
-                    </>
-                  ) : (
-                    <i>No Name</i>
-                  )}{" "}
-                  {contact.favorite ? (
-                    <span>★</span>
-                  ) : null}
-                </Link>
-              </li>
+                <li key={contact.id}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                          ? "pending"
+                          : ""
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite ? (
+                      <span>★</span>
+                    ) : null}
+                  </NavLink>
+                </li>
               ))}
             </ul>
             ) : (
